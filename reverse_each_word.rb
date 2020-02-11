5fc6a79265c081dd22efe99762a64d9d49710ddb
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-11 20:24:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,11 @@ def reverse_each_word(string)
   i = 0;
   array1 = []
   array2 = []
+  string
   array1 = string.split(" ")
   array1.each do |word|
     array2.push(word.reverse)
-    
+  end
+  string = array2.join(" ")
+  return string
+end
